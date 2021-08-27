3c91e35d31eec40c07d61ce117bfedb61ee55f3e
GiaRos33 <89648154+GiaRos33@users.noreply.github.com>
2021-08-27 17:34:16 +0200

    Added a # at line 6

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ include_attribute "kagent"
 
 default["tensorflow"]["version"]                 = "2.4.1"
 default['tensorflow']['serving']['version']      = "2.4.0"
-
+#
 #
 # TensorFlow/PyTorch example notebooks and datasets
 #
